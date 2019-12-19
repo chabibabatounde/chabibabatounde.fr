1b4e581df75ab89b7d073527322d92e06f573f31
Rodolpho BABATOUNDE <chabibabatounde@gmail.com>
2019-12-19 01:10:09 +0100

    A propos V1

---
+++ api/controllers/home.js
@@ -0,0 +1,16 @@
+/**
+ * Module dependencies
+ */
+
+// ...
+
+
+/**
+ * home.js
+ *
+ * Home something.
+ */
+module.exports = async function home(req, res) {
+  sails.log.debug('TODO: implement');
+  return res.view('home.ejs');
+};
